979d7141e0835a00b9e8a3543878404a0ba08a8b
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-07-05 17:31:21 +0200

    feat: Use current organization ID when working with shopping carts

--- src/VirtoCommerce.XCatalog.Core/Queries/CatalogQueryBase.cs
+++ src/VirtoCommerce.XCatalog.Core/Queries/CatalogQueryBase.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using GraphQL;
 using GraphQL.Types;
+using VirtoCommerce.StoreModule.Core.Model;
 using VirtoCommerce.Xapi.Core.BaseQueries;
 using VirtoCommerce.Xapi.Core.Extensions;
-using VirtoCommerce.StoreModule.Core.Model;
 using VirtoCommerce.XCatalog.Core.Models;
 
 namespace VirtoCommerce.XCatalog.Core.Queries
@@ -15,7 +15,7 @@ namespace VirtoCommerce.XCatalog.Core.Queries
         public string UserId { get; set; }
         public string CultureName { get; set; }
         public string CurrencyCode { get; set; }
-
+        public string OrganizationId { get; set; }
         public Store Store { get; set; }
         public IList<string> IncludeFields { get; set; } = Array.Empty<string>();
 
@@ -31,6 +31,7 @@ namespace VirtoCommerce.XCatalog.Core.Queries
         {
             StoreId = context.GetArgument<string>(nameof(StoreId));
             UserId = context.GetArgument<string>(nameof(UserId)) ?? context.GetCurrentUserId();
+            OrganizationId = context.GetCurrentOrganizationId();
             CultureName = context.GetArgument<string>(nameof(CultureName));
             CurrencyCode = context.GetArgument<string>(nameof(CurrencyCode));
         }
