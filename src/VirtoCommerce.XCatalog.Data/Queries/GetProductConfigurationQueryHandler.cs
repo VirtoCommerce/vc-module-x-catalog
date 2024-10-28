0341150545b32fcf73c62447fa49de27cc5d2c9b
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-10-28 15:11:55 +0200

    feat: mock improvements

--- src/VirtoCommerce.XCatalog.Data/Queries/GetProductConfigurationQueryHandler.cs
+++ src/VirtoCommerce.XCatalog.Data/Queries/GetProductConfigurationQueryHandler.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -13,6 +15,8 @@ public class GetProductConfigurationQueryHandler : IQueryHandler<GetProductConfi
 {
     private readonly IMediator _mediator;
 
+    private const string _productsFieldName = $"{nameof(ConfigurationQueryResponse.ConfigurationSections)}.{nameof(ExpConfigurationSection.Products)}.";
+
     public GetProductConfigurationQueryHandler(IMediator mediator)
     {
         _mediator = mediator;
@@ -24,33 +28,42 @@ public class GetProductConfigurationQueryHandler : IQueryHandler<GetProductConfi
         {
             ConfigurationSections = new List<ExpConfigurationSection>
             {
-                new ExpConfigurationSection
+                new()
                 {
                     Name = "Beverages",
+                    IsRequired = true,
                 },
-                new ExpConfigurationSection
+                new()
                 {
                     Name = "Baloons",
                 },
-                new ExpConfigurationSection
+                new()
                 {
                     Name = "Chips and Snacks"
                 }
             }
         };
 
+
+        var includeFields = request.IncludeFields
+                .Where(x => x.StartsWith(_productsFieldName, StringComparison.OrdinalIgnoreCase))
+                .Select(x => x.Replace(_productsFieldName, string.Empty, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+        var productsRequest = new SearchProductQuery()
+        {
+            StoreId = request?.StoreId,
+            CultureName = request?.CultureName,
+            CurrencyCode = request?.CurrencyCode,
+            UserId = request?.UserId,
+            OrganizationId = request?.OrganizationId,
+            IncludeFields = includeFields,
+            Take = 3,
+        };
+
         foreach (var section in result.ConfigurationSections)
         {
-            var productsRequest = new SearchProductQuery()
-            {
-                StoreId = request?.StoreId,
-                CultureName = request?.CultureName,
-                CurrencyCode = request?.CurrencyCode,
-                UserId = request?.UserId,
-                OrganizationId = request?.OrganizationId,
-                Take = 3,
-                Skip = 3 * result.ConfigurationSections.IndexOf(section),
-            };
+            productsRequest.Skip = 3 * result.ConfigurationSections.IndexOf(section);
 
             var productsResponse = await _mediator.Send(productsRequest, cancellationToken);
             section.Products = productsResponse.Results;
