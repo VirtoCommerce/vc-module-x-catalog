78a10fd2e5c632b942044490402c9a26b907519b
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-11-21 14:34:52 +0200

    fix: sonar

--- src/VirtoCommerce.XCatalog.Core/Models/ConfigurationQueryResponse.cs
+++
@@ -1,8 +0,0 @@
-//using System.Collections.Generic;
-
-//namespace VirtoCommerce.XCatalog.Core.Models;
-//public class ConfigurationQueryResponse
-//{
-//    public IList<ExpConfigurationSection> ConfigurationSections { get; set; } = [];
-//}
-
