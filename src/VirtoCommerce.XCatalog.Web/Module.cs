e0ac3fbf14aa94ee86483398142b8c1cb94adafd
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-05-15 09:21:17 +0200

    feat: move Brand Enabled setting to Catalog

--- src/VirtoCommerce.XCatalog.Web/Module.cs
+++ src/VirtoCommerce.XCatalog.Web/Module.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.Platform.Core.Modularity;
-using VirtoCommerce.Platform.Core.Settings;
-using VirtoCommerce.StoreModule.Core.Model;
 using VirtoCommerce.Xapi.Core.Extensions;
 using VirtoCommerce.XCatalog.Core;
 using VirtoCommerce.XCatalog.Data;
@@ -29,12 +27,6 @@ public class Module : IModule, IHasConfiguration
     public void PostInitialize(IApplicationBuilder appBuilder)
     {
         appBuilder.UseScopedSchema<DataAssemblyMarker>("catalog");
-
-        // settings
-        var serviceProvider = appBuilder.ApplicationServices;
-        var settingsRegistrar = serviceProvider.GetRequiredService<ISettingsRegistrar>();
-        settingsRegistrar.RegisterSettings(ModuleConstants.Settings.AllSettings, ModuleInfo.Id);
-        settingsRegistrar.RegisterSettingsForType(ModuleConstants.Settings.StoreLevelSettings, nameof(Store));
     }
 
     public void Uninstall()
