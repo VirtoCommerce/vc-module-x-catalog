2be80acc46c01eef5b58a1ac5a293e228880ec89
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2025-06-24 15:48:06 +0300

    Use AbstractTypeFactory to create a SearchRequest (#53)
    * Use AbstractTypeFactory to create a SearchRequest
    
    * Review fixes
    
    * Use AbstractTypeFactory to create SearchProductResponse and SearchCategoryResponse
    
    * Fix Sonar issue
    
    * Rename to OverridableType

---
+++ src/VirtoCommerce.XCatalog.Data/Extensions/OverridableType.cs
@@ -0,0 +1,9 @@
+﻿using VirtoCommerce.Platform.Core.Common;
+
+namespace VirtoCommerce.XCatalog.Data.Extensions;
+
+internal static class OverridableType<T> where T : new()
+{
+    //TODO: Move to AbstractTypeFactory<T> when it will be implemented
+    public static T New() => AbstractTypeFactory<T>.HasOverrides ? AbstractTypeFactory<T>.TryCreateInstance() : new T();
+}
