f7c9940a677f0278e63f42f6884cb979582bc2dc
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-07-03 11:50:48 +0200

    feat: check for store access for suggestions and child categories query

--- src/VirtoCommerce.XCatalog.Data/Queries/ProductSuggestionsQueryBuilder.cs
+++ src/VirtoCommerce.XCatalog.Data/Queries/ProductSuggestionsQueryBuilder.cs
@@ -1,6 +1,11 @@
+using System.Threading.Tasks;
+using GraphQL;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.StoreModule.Core.Services;
 using VirtoCommerce.Xapi.Core.BaseQueries;
+using VirtoCommerce.XCatalog.Core.Authorization;
 using VirtoCommerce.XCatalog.Core.Models;
 using VirtoCommerce.XCatalog.Core.Queries;
 using VirtoCommerce.XCatalog.Core.Schemas;
@@ -11,8 +16,24 @@ public class ProductSuggestionsQueryBuilder : QueryBuilder<ProductSuggestionsQue
 {
     protected override string Name => "ProductSuggestions";
 
-    public ProductSuggestionsQueryBuilder(IMediator mediator, IAuthorizationService authorizationService)
+    private readonly IStoreService _storeService;
+
+    public ProductSuggestionsQueryBuilder(IMediator mediator, IAuthorizationService authorizationService, IStoreService storeService)
         : base(mediator, authorizationService)
     {
+        _storeService = storeService;
+    }
+
+    protected override async Task BeforeMediatorSend(IResolveFieldContext<object> context, ProductSuggestionsQuery request)
+    {
+        await base.BeforeMediatorSend(context, request);
+
+        if (!string.IsNullOrEmpty(request.StoreId))
+        {
+            var store = await _storeService.GetByIdAsync(request.StoreId);
+            request.Store = store;
+        }
+
+        await Authorize(context, request.Store, new CanAccessStoreAuthorizationRequirement());
     }
 }
