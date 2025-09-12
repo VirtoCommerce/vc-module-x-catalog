d1eca5dce772c7acd3cb1a6123335e6dab212af4
Basil Kotov <basilkot@gmail.com>
2025-09-12 14:31:19 +0100

    remove linked categories when seo links type is collapsed

--- src/VirtoCommerce.XCatalog.Data/Queries/ChildCategoriesQueryBuilder.cs
+++ src/VirtoCommerce.XCatalog.Data/Queries/ChildCategoriesQueryBuilder.cs
@@ -4,18 +4,17 @@ using System.Threading.Tasks;
 using GraphQL;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using VirtoCommerce.CatalogModule.Core.Extensions;
 using VirtoCommerce.CatalogModule.Core.Model;
 using VirtoCommerce.CatalogModule.Core.Services;
 using VirtoCommerce.CoreModule.Core.Currency;
 using VirtoCommerce.Platform.Core.Common;
-using VirtoCommerce.StoreModule.Core.Extensions;
 using VirtoCommerce.StoreModule.Core.Services;
 using VirtoCommerce.XCatalog.Core.Authorization;
 using VirtoCommerce.XCatalog.Core.Extensions;
 using VirtoCommerce.XCatalog.Core.Models;
 using VirtoCommerce.XCatalog.Core.Queries;
 using VirtoCommerce.XCatalog.Core.Schemas;
-using static VirtoCommerce.StoreModule.Core.ModuleConstants.Settings.SEO;
 
 namespace VirtoCommerce.XCatalog.Data.Queries;
 
@@ -64,19 +63,12 @@ public class ChildCategoriesQueryBuilder : CatalogQueryBuilder<ChildCategoriesQu
             var responseGroup = GetCategoryResponseGroup(context, request, response);
             var categoriesByIds = new Dictionary<string, Category>();
             var store = request.Store;
-
             var storeCatalog = await _catalogService.GetByIdAsync(store.Catalog);
-            var isCollapsed = store.GetSeoLinksType() == SeoCollapsed && storeCatalog.IsVirtual;
 
             foreach (var idsBatch in categoryIds.Paginate(_batchSize))
             {
                 IEnumerable<Category> categories = await _categoryService.GetAsync(idsBatch, responseGroup);
-
-                if (isCollapsed)
-                {
-                    categories = categories.Where(x => x.Links.All(link => link.CatalogId != store.Catalog));
-                }
-
+                categories = categories.FilterLinked(store, storeCatalog);
                 categoriesByIds.AddRange(categories.ToDictionary(x => x.Id));
             }
 
@@ -87,11 +79,25 @@ public class ChildCategoriesQueryBuilder : CatalogQueryBuilder<ChildCategoriesQu
         }
 
         // Cannot reassign the root.ChildCategories because it is referenced in the response.
+        RemoveUnsuitableCategories(root);
+    }
+
+    private void RemoveUnsuitableCategories(ExpCategory root)
+    {
+        if (root.ChildCategories == null)
+        {
+            return;
+        }
         var itemsToRemove = root.ChildCategories.Where(x => x.Category == null).ToList();
 
         foreach (var category in itemsToRemove)
         {
             root.ChildCategories.Remove(category);
+        }
+
+        foreach (var category in root.ChildCategories)
+        {
+            RemoveUnsuitableCategories(category);
         }
     }
 
