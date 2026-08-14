502608b0ed738cb00178969944d9d6aa252c26b5
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2026-08-14 16:08:20 +0200

    VCST-5691: Create ProductPrice with AbstractTypeFactory (#109)
    (cherry picked from commit 8a63bdb96e1edc41de4b647f7a11a65224c4c43c)

--- src/VirtoCommerce.XCatalog.Data/Mapping/ProductMappingProfile.cs
+++ src/VirtoCommerce.XCatalog.Data/Mapping/ProductMappingProfile.cs
@@ -113,15 +113,12 @@ namespace VirtoCommerce.XCatalog.Data.Mapping
                         var currency = allCurrencies[price.Currency];
                         if (currency != null)
                         {
-                            var productPrice = new ProductPrice(currency)
-                            {
-                                ProductId = price.ProductId,
-                                PricelistId = price.PricelistId,
-                                Currency = currency,
-                                StartDate = price.StartDate,
-                                EndDate = price.EndDate,
-                                ListPrice = new Money(price.List, currency)
-                            };
+                            var productPrice = AbstractTypeFactory<ProductPrice>.TryCreateInstance(nameof(ProductPrice), currency);
+                            productPrice.ProductId = price.ProductId;
+                            productPrice.PricelistId = price.PricelistId;
+                            productPrice.StartDate = price.StartDate;
+                            productPrice.EndDate = price.EndDate;
+                            productPrice.ListPrice = new Money(price.List, currency);
                             productPrice.SalePrice = price.Sale == null ? productPrice.ListPrice : new Money(price.Sale ?? 0m, currency);
                             productPrice.MinQuantity = price.MinQuantity;
 
